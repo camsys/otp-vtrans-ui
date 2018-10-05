750d0040cbb74ae51428196ff18cef628d3b17a2
Simon Jacobs <sidaja@gmail.com>
2018-10-05 14:10:32 -0400

    just use average

--- src/lib/leaflet-shifted-polyline.js
+++ src/lib/leaflet-shifted-polyline.js
@@ -39,25 +39,19 @@ L.ShiftedPolyline = L.Polyline.extend({
             angles[i] = theta_p;
         }
         for (var i = 0; i < points.length; i++) {
-            var theta1, theta2;
+            var theta;
             if (i == points.length - 1) {
-                theta1 = angles[i - 1];
+                theta = angles[i - 1];
+            } else if (i == 0) {
+                theta = angles[0];
             } else {
-                theta1 = angles[i];
+                theta = (angles[i - 1] + angles[i]) / 2.0;
             }
             var p = points[i];
-            var x1 = p.x + delta * Math.cos(theta1);
-            var y1 = p.y + delta * Math.sin(theta1);
+            var x = p.x + delta * Math.cos(theta);
+            var y = p.y + delta * Math.sin(theta);
 
-            if (i > 0 && i < points.length - 1) {
-                theta2 = angles[i - 1];
-                var x2 = p.x + delta * Math.cos(theta2);
-                var y2 = p.y + delta * Math.sin(theta2);
-                x1 = (x1 + x2) / 2;
-                y1 = (y1 + y2) / 2;
-            }
-
-            new_points.push(L.point(x1, y1));
+            new_points.push(L.point(x, y));
         }
         return new_points;
     }
