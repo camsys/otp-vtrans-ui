b90998233bec2ef1a3d88a585aaebbadb5eed535
mmaranda <mmaranda@camsys.com>
2020-02-20 15:44:31 -0500

    OCOTP-126 This adds the link to the Trip Planner words. Which will link to http://www.transitorange.info/

--- package_release_qa.sh
+++ package_release_qa.sh
@@ -1,5 +1,5 @@
 git pull
-cp ~/workspace/vtrans/otp-vtrans-ui/vtrans_ui_config_qa.js ~/workspace/vtrans/otp-vtrans-ui/src/config.js
+cp ~/workspace/vtrans_ui/otp-vtrans-ui/vtrans_ui_config_qa.js ~/workspace/vtrans_ui/otp-vtrans-ui/src/config.js
 npm install
 npm run build
 tar -cvzf oc_otp_ui.tgz dist
